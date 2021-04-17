7b7c6dd7e5349c4166eab4b4ccc86ec9e0ef4141
numhebzaba <num.ptp@gmail.com>
2021-04-17 21:29:08 +0700

    no message

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace homework_Pascal_s_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floor = int.Parse(Console.ReadLine());
+            if (floor>=0)
+            {
+                for (int i= 0; i <= floor; i++)
+                {
+                    for(int j = 0; j <= i; j++)
+                    {
+                        Console.Write(" ");
+                        Console.Write(Combination(i, j));
+                        Console.Write(" ");
+
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Pascal’s triangle row number.");
+            }
+            Console.ReadLine();
+        }
+
+        static int Factorial(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+            return n * Factorial(n - 1);
+        }
+        static int Combination(int n,int r)
+        {
+            return Factorial(n) / (Factorial(n - r) * Factorial(r));
+        }
+    }
+}
